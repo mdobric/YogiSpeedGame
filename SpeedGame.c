7fe5a63da7fc3721d1122a9c315977a260d6dee5
Dobric, Mirza <Mirza.Dobric@Honeywell.com>
2018-12-19 17:40:09 -0500

    allocate structs on heap from setup method

--- SpeedGame.c
+++ SpeedGame.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 // EVENTS
 #define TIMER_EXPIRED 0
 #define SWITCH_TRIGGERED 1
@@ -9,16 +10,16 @@
 #define FAIL_STATE 3
 
 struct State {
-  int stateDuration;
-  int* melody;
-  int noteCount;
+  unsigned int stateDuration;
+  unsigned int* melody;
+  unsigned int noteCount;
 };
 
 struct Machine {
   int currentState;
-  int timeInCurrentState;
-  struct State* states;
-  int stateCount;
+  unsigned int timeInCurrentState;
+  struct State** states;
+  unsigned int stateCount;
 };
 
 int getCurrentNote(struct State* state, int currentTime) {
@@ -35,12 +36,23 @@ int getCurrentNote(struct State* state, int currentTime) {
   return state->melody[2 * currentNote];
 }
 
+struct Machine* stateMachine;
+
+void setup() {
+  stateMachine = (struct Machine*) malloc(sizeof(struct Machine));
+  struct State* readyState = (struct State*) malloc(sizeof(struct State));
+  readyState->stateDuration = 12;
+  readyState->noteCount = 4;
+  readyState->melody = (unsigned int*) malloc(sizeof(unsigned int) * readyState->noteCount);
+  readyState->melody[0] = 400;
+  readyState->melody[1] = 4;
+  readyState->melody[2] = 800;
+  readyState->melody[3] = 8;
+  stateMachine->states = (struct State**) malloc(sizeof(struct State*)*1);
+  stateMachine->states[0] = readyState;
+}
+
 int main() {
-  struct State* state;
-  state->stateDuration = 12;
-  int melody[] = { 400, 4, 800, 8 };
-  state->melody = melody;
-  state->noteCount = 4;
-  
+  setup();
   return 0;
 }
