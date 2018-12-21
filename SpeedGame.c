e1a99594c615a67ad8049aeb040b2c2bf1df28ef
Dobric, Mirza <Mirza.Dobric@Honeywell.com>
2018-12-21 14:30:55 -0500

    minor refactoring; first take on tick() function

--- SpeedGame.c
+++ SpeedGame.c
@@ -10,7 +10,7 @@
 #define FAIL_STATE 3
 
 struct State {
-  unsigned int stateDuration;
+  unsigned int duration;
   unsigned int* melody;
   unsigned int noteCount;
 };
@@ -22,7 +22,19 @@ struct Machine {
   unsigned int stateCount;
 };
 
-int getCurrentNote(struct State* state, int currentTime) {
+unsigned int currentNote = 0;
+
+void playNote(unsigned int note) {
+  if (currentNote == note) return;
+  currentNote = note;
+  if (currentNote == 0) {
+    // noTone(TONE_PIN);
+  } else {
+    // tone(TONE_PIN, currentNote);
+  }
+}
+
+unsigned int getCurrentNote(struct State* state, int currentTime) {
   int currentMelodyTimePoint = currentTime % state->melody[2 * state->noteCount - 1];
   int currentNote = 0;
   int endTime = state->melody[2 * currentNote + 1];
@@ -36,38 +48,51 @@ int getCurrentNote(struct State* state, int currentTime) {
   return state->melody[2 * currentNote];
 }
 
+void tick(struct Machine* machine) {
+  struct State* currentState = machine->states[machine->currentState];  
+  if (machine->timeInCurrentState < currentState->duration) {
+    // stay in current state; get tone
+    unsigned int currentNote = getCurrentNote(currentState, machine->timeInCurrentState);
+    playNote(currentNote);
+  }
+}
+
 struct Machine* stateMachine;
 
 void setup() {
   stateMachine = (struct Machine*) malloc(sizeof(struct Machine));
   struct State* readyState = (struct State*) malloc(sizeof(struct State));
-  readyState->stateDuration = 12;
+  readyState->duration = 12;
   readyState->noteCount = 4;
   readyState->melody = (unsigned int*) malloc(sizeof(unsigned int) * readyState->noteCount);
   readyState->melody[0] = 400;
   readyState->melody[1] = 4;
   readyState->melody[2] = 800;
   readyState->melody[3] = 8;
-  stateMachine->states = (struct State**) malloc(sizeof(struct State*)*4);
+  stateMachine->stateCount = 4;
+  stateMachine->states = (struct State**) malloc(sizeof(struct State*) * stateMachine->stateCount);
   stateMachine->states[READY_STATE] = readyState;
 
   struct State* notReadyState = (struct State*) malloc(sizeof(struct State));
-  notReadyState->stateDuration = 12;
+  notReadyState->duration = 12;
   notReadyState->noteCount = 0;
   notReadyState->melody = NULL;
   stateMachine->states[NOT_READY_STATE] = notReadyState;
 
   struct State* successState = (struct State*) malloc(sizeof(struct State));
-  successState->stateDuration = 12;
+  successState->duration = 12;
   successState->noteCount = 0;
   successState->melody = NULL;
   stateMachine->states[SUCCESS_STATE] = successState;
 
   struct State* failState = (struct State*) malloc(sizeof(struct State));
-  failState->stateDuration = 12;
+  failState->duration = 12;
   failState->noteCount = 0;
   failState->melody = NULL;
   stateMachine->states[FAIL_STATE] = failState;
+
+  stateMachine->currentState = READY_STATE;
+  stateMachine->timeInCurrentState = 0;
 }
 
 int main() {
